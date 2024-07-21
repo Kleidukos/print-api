df4f748e4011313d84aa3d1ffbbe960508636a1f
Hécate Kleidukos <hecate+github@glitchbra.in>
2024-07-21 13:27:57 +0200

    Support GHC 9.8.2

---
+++ compat/9.8.2/GHC/Compat.hs
@@ -0,0 +1,16 @@
+-- GHC 9.10.1 Compatibility
+module GHC.Compat where
+
+import GHC (ModuleInfo, modInfoExports)
+import GHC.Iface.Syntax (AltPpr (..), ShowForAllFlag (..), ShowHowMuch (..), ShowSub (..))
+import GHC.Types.Name (OccName, nameOccName)
+
+import DumpDecls.IgnoredDeclarations
+
+mkShowSub :: ModuleInfo -> ShowSub
+mkShowSub mod_info =
+  let ss_how_much = ShowSome [] (AltPpr Nothing)
+   in ShowSub
+        { ss_how_much = ss_how_much
+        , ss_forall = ShowForAllMust
+        }
