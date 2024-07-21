d2d564160b2cc4c38b2d6bf8d10e1cdf5a0adbe1
Hécate Kleidukos <hecate+github@glitchbra.in>
2024-07-21 20:28:11 +0200

    Use cabal at run-time to know the libdir of the GHC install

--- src/PrintApi/CLI/Types.hs
+++ src/PrintApi/CLI/Types.hs
@@ -1,10 +1,13 @@
 module PrintApi.CLI.Types where
 
+import Data.ByteString.Lazy.Char8 qualified as ByteString
+import Data.List.Extra qualified as List
 import Data.Version (showVersion)
-import GHC.Paths (libdir)
 import Options.Applicative
 import Paths_print_api (version)
 import PrintApi.CLI.Cmd.Dump (run)
+import System.Process.Typed (ExitCode (..))
+import System.Process.Typed qualified as Process
 
 data Options = Options
   { packageName :: String
@@ -22,7 +25,13 @@ runOptions
   :: Options
   -> IO ()
 runOptions (Options packageName) = do
-  run libdir packageName
+  (exitCode, stdOut, stdErr) <- Process.readProcess $ Process.shell "cabal exec -v0 -- ghc --print-libdir"
+  case exitCode of
+    ExitSuccess ->
+      run (List.trimEnd $ ByteString.unpack stdOut) packageName
+    ExitFailure int -> do
+      putStrLn $ "`cabal exec -v0 -- ghc --print-libdir` exited with error code " <> show int
+      error $ ByteString.unpack stdErr
 
 withInfo :: Parser a -> String -> ParserInfo a
 withInfo opts desc =
