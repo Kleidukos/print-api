abc8861c5e437280966aa09048aa080a03437ee5
Hécate <Kleidukos@users.noreply.github.com>
2024-08-21 21:23:59 +0200

    Put the newly-built print-api in the PATH of the CI runner (#19)

--- test/IgnoreList.hs
+++ test/IgnoreList.hs
@@ -14,10 +14,12 @@ import Utils
 import qualified Data.ByteString.Lazy as ByteString
 import qualified System.Directory as Directory 
 
-import qualified PrintApi.CLI.Cmd.Dump as Dump
 import qualified System.Directory.OsPath as OsPath
 import qualified System.IO as System
 import qualified System.OsPath as OsPath
+import Control.Monad.Extra (whenM)
+
+import qualified PrintApi.CLI.Cmd.Dump as Dump
 
 diffCmd :: String -> String -> [String]
 diffCmd ref new = ["diff", "-u", ref, new]
@@ -27,23 +29,26 @@ spec = testGroup "Ignore list"
   [ goldenVsStringDiff
         "User-supplied ignore list"
         diffCmd
-        "test/golden/servant-client-expected-api.txt"
-        generateServantClientAPIWithIgnoreList
+        "test/golden/vector-expected-api.txt"
+        generateVectorAPIWithIgnoreList
   ]
 
-generateServantClientAPIWithIgnoreList :: (MonadIO m) => m LazyByteString
-generateServantClientAPIWithIgnoreList = do
+generateVectorAPIWithIgnoreList :: (MonadIO m) => m LazyByteString
+generateVectorAPIWithIgnoreList = do
+  let vectorPath = "../vector-0.13.1.0"
+  liftIO $ whenM (Directory.doesDirectoryExist vectorPath) $ 
+    Directory.removeDirectoryRecursive vectorPath
   (exitCode, stdOut, _stdErr) <- Process.readProcess $ Process.shell "cabal exec -v0 -- ghc --print-libdir"
   assertExitSuccess "`cabal exec -v0 -- ghc --print-libdir`" exitCode
-  assertExitSuccess "Fetch the archive of servant-client" =<< Process.runProcess (Process.shell "cabal get servant-client-0.20 --destdir=../")
-  liftIO $ Directory.setCurrentDirectory "../servant-client-0.20"
-  let buildServantClient = Process.shell "cabal build -j --allow-newer --write-ghc-environment-files=always"
-  assertExitSuccess "Build servant-client" =<< Process.runProcess buildServantClient
-  ignoreListPath <- liftIO $ OsPath.makeAbsolute [osp|../print-api/test/golden/servant-client-ignore-list.txt|]
+  assertExitSuccess "Fetch the archive of vector" =<< Process.runProcess (Process.shell "cabal get vector-0.13.1.0 --destdir=../")
+  liftIO $ Directory.setCurrentDirectory vectorPath
+  let buildVector = Process.shell "cabal build -j --write-ghc-environment-files=always"
+  assertExitSuccess "Build vector" =<< Process.runProcess buildVector
+  ignoreListPath <- liftIO $ OsPath.makeAbsolute [osp|../print-api/test/golden/vector-ignore-list.txt|]
   ignoreListFilePath <- liftIO $ OsPath.decodeUtf ignoreListPath
   modules <- lines <$> liftIO (System.readFile ignoreListFilePath)
   let ignoredModules = List.map mkModuleName modules
-  actualAPI <- liftIO $ Dump.computePackageAPI (List.trimEnd $ C8.unpack stdOut) ignoredModules "servant-client"
-  actualApiPath <- liftIO $ Directory.makeAbsolute "../print-api/test/golden/servant-client-actual-api.txt"
+  actualAPI <- liftIO $ Dump.computePackageAPI (List.trimEnd $ C8.unpack stdOut) ignoredModules "vector"
+  actualApiPath <- liftIO $ Directory.makeAbsolute "../print-api/test/golden/vector-actual-api.txt"
   liftIO $ System.writeFile actualApiPath actualAPI
   liftIO $ ByteString.readFile actualApiPath
